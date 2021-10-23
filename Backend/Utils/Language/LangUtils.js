f8c833d3e70702d6b9acadefc322736a5872a8ee
sonerpyci <sonerpayci@gmail.com>
2021-10-24 01:58:34 +0300

    Multi-Language messages & logout endpoint implementation.

---
+++ Backend/Utils/Language/LangUtils.js
@@ -0,0 +1,106 @@
+module.exports = {
+    error_messages : {
+        "001": {
+            "TR" : "Kayıt Bulunamadı.",
+            "EN" : "Record Not Found."
+        },
+        "002": {
+            "TR" : "Bazı bilgiler gönderilmedi.",
+            "EN" : "Some information was not sent."
+        },
+        "003": {
+            "TR" : "Bu Email/Kullanıcı adı ile bir kullanıcı zaten kayıtlı.",
+            "EN" : "Email Address or Username Already Exists in Database For an User."
+        },
+        "004": {
+            "TR" : "Kullanıcı Adı/Email ya da Parola Hatalı.",
+            "EN" : "Username/Email or Password Incorrect."
+        },
+        "005": {
+            "TR" : "Anahtar imzalanırken Hata Oluştu.",
+            "EN" : "Error Signing Token."
+        },
+        "006": {
+            "TR" : "Mevcut bir oturum bulunmakta. Giriş yapabilmek için lütfen mevcut oturumunuzu sonlandırın.",
+            "EN" : "There is an existing session. To log in, please end your current session."
+        },
+        "007": {
+            "TR" : "Ip adresiniz tanımlanan IP adresi ile uygun değil. Lütfen destek birimine başvurun.",
+            "EN" : "Your IP address does not match the desired IP address. Please contact Help Center."
+        },
+        "008": {
+            "TR" : "Zaten çıkış yaptınız.",
+            "EN" : "Already logged out."
+        },
+        "009": {
+            "TR" : "Girilen Email bilgisi kayıtlarımız ile eşleşmedi.",
+            "EN" : "The Email information entered did not match any of our records."
+        },
+        "010": {
+            "TR" : "Yanlış Kurtarma Kodu.",
+            "EN" : "Wrong Recovery Code."
+        },
+        "011": {
+            "TR" : "Parola eski parolanızdan farklı olmalıdır.",
+            "EN" : "The password should be different from your old password."
+        },
+        "012": {
+            "TR" : "Bu dosya tipine izin verilmiyor.",
+            "EN" : "This file type is not allowed."
+        },
+        "013": {
+            "TR" : "Bu alana erişim yetkiniz bulunmamaktadır.",
+            "EN" : "You are not authorized to access this area."
+        },
+        "014": {
+            "TR" : "Oturum süresi uzatılırken bir hatayla karşılaşıldı.",
+            "EN" : "An error was encountered while extending session duration."
+        },
+    },
+    info_messages : {
+        "001": {
+            "TR": "Bu yeni bir kayıt değildi, çünkü benzer kayıt bugün için veritabanında zaten var.",
+            "EN": "This wasn't a new record, because the similar record already exists in database for today."
+        },
+        "002": {
+            "TR": "Bakımdayız.",
+            "EN": "We are in Maintenance."
+        },
+        "003": {
+            "TR" : "Başarıyla çıkış yapıldı.",
+            "EN" : "Successfully logged out."
+        },
+        "004": {
+            "TR" : "Tüm kullanıcılar sistemden düşürüldü ve yeniden giriş yapmaları engellendi.",
+            "EN" : "All users were dropped from the system and denied login again."
+        },
+        "005": {
+            "TR" : "Kullanıcılar artık giriş yapabilir.",
+            "EN" : "Users can now login."
+        },
+        "006": {
+            "TR" : "Lütfen E-postanızı kontrol ediniz.",
+            "EN" : "Please check your Mail Box."
+        },
+        "007": {
+            "TR" : "Kurtarma Kodu Bir hesapla eşleşti.",
+            "EN" : "Recovery Code Matched with an account."
+        },
+        "008": {
+            "TR" : "Parolanızı başarıyla sıfırladınız.",
+            "EN" : "You have successfully reset your password."
+        },
+        "009": {
+            "TR" : "Bu endpoint kullanımdan kaldırıldı!",
+            "EN" : "This endpoint won't work anymore!"
+        },
+        "010": {
+            "TR": "Bu bilgiler zaten kayıtlı. Mevcut kayıttaki verileri görmektesiniz.",
+            "EN": "This data already exists in our db. You currently see the existing record."
+        },
+        "011": {
+            "TR": "Mail başarıyla gönderildi.",
+            "EN": "The mail has been sent successfully."
+        }
+    }
+}
